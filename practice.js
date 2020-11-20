e29470cde3b869e970f298d2b3f96a9ff000c479
Elliot Faustino <elliot.faustino@gmail.com>
2020-11-19 20:51:38 -0800

    Day 3

---
+++ practice.js
@@ -0,0 +1,51 @@
+// this is a comment 😊😁😁
+
+// this is a variable
+let variable
+
+// this is a string
+""
+
+// this is an number
+10
+
+// these are Booleans
+true
+false
+
+let friend1 = "rylan"
+let friend2 = "jason"
+let friend3 = "liam"
+let friend4 = "leona"
+let friend5 = "esme"
+
+// this is an array
+// indexes   0        1        2        3        4
+let array = [friend1, friend2, friend3, friend4, friend5]
+
+// console.log(array[4] + " " + "💁‍♀️")
+
+// these are array methods
+// console.log(array.reverse())
+array.push("joshua")
+// array.splice(3, 1)
+
+const statements = ["awesome", "nice", "cool", "dank", "super", "duper"]
+// this is a loop
+array.forEach((element, index) => {
+    console.log(element + " is " + statements[index])
+})
+
+// let greeting = "hi human"
+// let greeting2 = "my name is"
+// let name = "Elliot"
+
+// console.log("hello," + "my name is" + "frank")
+// console.log(greeting +  greeting2 + name)
+
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
